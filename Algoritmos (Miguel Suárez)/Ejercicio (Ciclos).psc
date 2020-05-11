22ee5443fa4e516b077949ce40bac0455f48ae61
Alec <aleclux4@hotmail.com>
2020-05-11 11:28:41 -0500

    Actualización Scrum

--- Algoritmos (Miguel Suárez)/Ejercicio (Ciclos).psc
+++
@@ -1,6 +0,0 @@
-Algoritmo vf
-	v=0
-	Para v<-1 Hasta 10 Con Paso 1 Hacer
-		Escribir v
-	FinPara
-FinAlgoritmo
--- Arquitectura del Sistema (Eder Lara)/alecjuan/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-         # Esto es un comentario
-         echo "Hola Mundo <br>";
-         echo Date('y-m-d');
-?>?>
-<section>
-  <?php echo "<h1>Hola Clase<h1>";?>
-        </section>
